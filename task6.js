2fe8c245c578d074e882ba2907f6f25e02b52455
motiurrahman124 <motiurr503@gmail.com>
2023-07-26 01:25:01 +0600

    task8 is done

--- task6.js
+++ task6.js
@@ -1,6 +1,6 @@
 // Task 6: Create a program that generates a random password of a specified length. The password should include a mix of uppercase letters, lowercase letters, numbers, and special characters.
 
-function generatePassword(length) {
+const generatePassword=(length)=>{
   const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const lowerCase = "abcdefghijklmnopqrstuvwxyz";
   const numbers = "0123456789";
